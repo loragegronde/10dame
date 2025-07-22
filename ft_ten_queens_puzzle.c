3ab21522f3adb625b299a930e7160a280e7e7058
loragegronde <campionauguste@gmail.com>
2025-07-22 02:22:53 +0200

    Update ft_ten_queens_puzzle.c

--- ft_ten_queens_puzzle.c
+++ ft_ten_queens_puzzle.c
@@ -9,130 +9,84 @@
 /*   Updated: 2025/07/21 22:31:48 by acampion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
-void	write_queen(int grid[10][10], int *count);
-int		ft_ten_queens_puzzle(void);
-void	create_grid(int grid[10][10]);
-int		check_col_row_diag(int grid[10][10], int row, int col);
-int		solve(int grid[10][10], int pos, int num_q, int *count);
-int		put_queen(int grid[10][10], int pos, int num_q);
-
-int	ft_ten_queens_puzzle(void)
+// Vérifie si une reine peut être placée à la position (row, col) sans conflit avec les autres
+int	is_safe(int queens[10], int row, int col)
 {
-	int	grid[10][10];
-	int	count;
-
-	count = 0;
-	create_grid(grid);
-	solve(grid, 0, 0, &count);
-	return (count);
-}
-
-void	create_grid(int grid[10][10])
-{
-	int	row;
-	int	col;
-
-	col = 0;
-	while (col < 10)
-	{
-		row = 0;
-		while (row < 10)
-		{
-			grid[row][col] = 0;
-			row++;
-		}
-		col++;
-	}
-}
-
-int	check_col_row_diag(int grid[10][10], int row, int col)
-{
-	int	i;
-
+	int i;
 	i = 0;
-	while (i < 10)
+	while (i < row)
 	{
-		if (grid[row][i] == 1 || grid[i][col] == 1)
-			return (0);
-		i++;
-	}
-	i = 1;
-	while (row - i >= 0 && col + i < 10)
-	{
-		if (grid[row - i][col + i] == 1)
-			return (0);
-		i++;
-	}
-	i = 1;
-	while (row - i >= 0 && col - i >= 0)
-	{
-		if (grid[row - i][col - i] == 1)
+		// Vérifie la colonne et les deux diagonales
+		if (queens[i] == col || (i - row) == (queens[i] - col) || (row - i) == (queens[i] - col))
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-int	solve(int grid[10][10], int pos, int num_q, int *count)
+// Affiche une solution valide (une position possible des 10 reines)
+// et incrémente le compteur de solutions
+void	write_queen(int queens[10], int *count)
 {
-	int	row;
-	int	col;
-
-	if (num_q == 10)
-	{
-		write_queen(grid, count);
-		return (0);
-	}
-	if (pos >= 100)
-		return (0);
-	row = pos / 10;
-	col = pos % 10;
-	if (row > num_q)
-		return(0);
-	if (row == num_q && grid[row][col] == 0 &&
-			check_col_row_diag(grid, row, col))
-	{
-		grid[row][col] = 1;
-		solve(grid, pos + 1, num_q + 1, count);
-		grid[row][col] = 0;
-	}
-	else
-		solve(grid, pos + 1, num_q, count);
-	return (0);
-}
-
-void	write_queen(int grid[10][10], int *count)
-{
-	int		i;
-	int		j;
-	char	c;
-
+	int i;
+	char c;
 	i = 0;
 	while (i < 10)
 	{
-		j = 0;
-		while (j < 10)
-		{
-			if (grid[i][j] == 1)
-			{
-				c = j + '0';
-				write(1, &c, 1);
-			}
-			j++;
-		}
+		// Affiche la colonne de chaque reine pour chaque ligne
+		c = queens[i] + '0';
+		write(1, &c, 1);
 		i++;
 	}
 	write(1, "\n", 1);
 	(*count)++;
 }
 
+// Place les reines une par une, ligne par ligne, de façon récursive
+void	solve(int queens[10], int row, int *count)
+{
+	int col;
+	// Si on a placé 10 reines, on affiche la solution
+	if (row == 10)
+	{
+		write_queen(queens, count);
+		return;
+	}
+	col = 0;
+	while (col < 10)
+	{
+		// Tente de placer une reine à la colonne col si c'est possible
+		if (is_safe(queens, row, col))
+		{
+			queens[row] = col;
+			// Passe à la ligne suivante
+			solve(queens, row + 1, count);
+		}
+		col++;
+	}
+}
+
+// Fonction principale qui initialise le tableau et lance la recherche
+int	ft_ten_queens_puzzle(void)
+{
+	int queens[10]; // queens[i] = colonne de la reine sur la ligne i
+	int count;
+	count = 0;
+	// Commence la résolution à partir de la première ligne
+	solve(queens, 0, &count);
+	return (count);
+}
+
+// Point d'entrée du programme
 int	main(void)
 {
 	int total;
-	
+	// Calcule le nombre total de solutions et l'affiche
 	total = ft_ten_queens_puzzle();
 	printf("Total solutions: %d\n", total);
+	return (0);
 }
